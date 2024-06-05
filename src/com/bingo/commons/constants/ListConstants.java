48de3aa9386ef04437e6ae88841212c4910d0338
nia <1205367274@qq.com>
2024-06-05 23:12:55 +0800

    List常量类

---
+++ src/com/bingo/commons/constants/ListConstants.java
@@ -0,0 +1,17 @@
+package com.bingo.commons.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author nia
+ * @description 常量列表
+ * @Date 2024/6/5
+ */
+public class ListConstants {
+    public static final List<String> START = Arrays.asList("登录账号", "注册账号", "退出");
+    public static final List<String> LOGIN = Arrays.asList("普通用户登录", "商家登录", "管理员登录","退出");
+    public static final List<String> REGISTER = Arrays.asList("普通用户注册", "商家注册","退出");
+    public static final List<String> PURCHASER = Arrays.asList("查看商品", "购物车", "Bin圈", "消息", "个人界面", "退出");
+
+}
